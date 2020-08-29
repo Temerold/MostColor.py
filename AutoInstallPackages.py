bfe60d3f0f7a137b2abea1c1fe2b269ddd160588
Temerold <67090612+Temerold@users.noreply.github.com>
2020-08-29 15:49:16 +0200

    Added AutoInstallPackages.py

---
+++ AutoInstallPackages.py
@@ -0,0 +1,8 @@
+import os
+try:
+    os.system("pip install PIL")
+    os.system("pip install scipy")
+    os.system("pip install numpy")
+    input("Installed packages successfully.")
+except:
+    input("Failed.")
