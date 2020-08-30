11ce3885e7ac4810c89926c558716d0dd3cf2c37
Temerold <67090612+Temerold@users.noreply.github.com>
2020-08-30 12:28:50 +0200

    Bug fix: Transparent color not seen as black

--- MostColor.py
+++ MostColor.py
@@ -28,51 +28,59 @@ dots4 = ['.webp', '.jpeg']
 for root, dirs, files in os.walk(root, topdown=False):
     for name in files:
         if name[-4:] in dots3 or name[-5:] in dots4:
-            print("File Path: " + root)
-            print("File Name: " + name)
-            print("\nReading image . . .")
-            im = Image.open(root + "\\" + name)
+            try:
+                print("File Path: " + root)
+                print("File Name: " + name)
+                print("\nReading image . . .")
+                im = Image.open(root + "\\" + name)
              
-            width, height = im.size
-            print("Image size: " + str(width) + " x " + str(height))
+                width, height = im.size
+                print("Image size: " + str(width) + " x " + str(height))
 
-            if im.size >= (150, 150):
-                im = im.resize((150, 150))
-            ar = np.asarray(im)
-            shape = ar.shape
-            try:
-                ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+                if im.size >= (150, 150):
+                    im = im.resize((150, 150))
+                    print("Resized image to 150 x 150.")
+                ar = np.asarray(im)
+                shape = ar.shape
+                try:
+                    ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+                except:
+                    pass
+
+                print("\nFinding clusters . . .")
+                try:
+                    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+                except:
+                    pass
+                print("\nCluster centres:\n", codes)
+
+                try:
+                    vecs, dist = scipy.cluster.vq.vq(ar, codes)
+                except:
+                    pass
+                counts, bins = np.histogram(vecs, len(codes))
+
+
+                index_max = np.argmax(counts)
+                peak = codes[index_max]
+                
+                colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+                if colour == "00000000":
+                    try:
+                        peak = codes[index_max - 1]
+                    except:
+                        pass
+                    colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+                print("\nMost frequent color is %s (#%s)" % (peak, colour))
+
+                
+
+                img = Image.new("RGB", (width, height), ImageColor.getrgb("#" + colour))
+                os.remove(root + "\\" + name)
+                img.save(root + "\\" + name)
+                # img.show() # don't - may crash PC
             except:
-                pass
-
-            print("\nFinding clusters . . .")
-            try:
-                codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-            except:
-                pass
-            print("\nCluster centres:\n", codes)
-
-            try:
-                vecs, dist = scipy.cluster.vq.vq(ar, codes)
-            except:
-                pass
-            counts, bins = np.histogram(vecs, len(codes))
-
-
-            index_max = np.argmax(counts)
-            peak = codes[index_max]
-            print(str(peak))
-            if str(peak)  == '[255. 255. 255.   0.]':
-                print(peak)
-                print(str(index_max))
-                peak = codes[index_max - 1]
-            colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
-            print("\nMost frequent color is %s (#%s)" % (peak, colour))
-
-
-            img = Image.new("RGB", (width, height), ImageColor.getrgb("#" + colour))
-            os.remove(root + "\\" + name)
-            img.save(root + "\\" + name)
-            # img.show() # don't - may crash PC
+                print("Operation failed (%s)" % root + "\\" + name)
+                
     if prompt_Stop == True:
         input(">>>")
