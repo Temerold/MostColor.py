74f1efe594aeaf00540a1bdba3cb9bdf0dc436a6
Temerold <67090612+Temerold@users.noreply.github.com>
2020-09-28 17:09:48 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Filepath to check if prompt_Root = True
+root = './images'
+
+# Program asks for path of images (True), or pre-set root (above) (False)
+promptRoot = True
+
+# Program says it's done when done (True), or instant close (False)
+promptStop = True
+
+# Program prints everything it does
+debug = True
+
+# Program writes all colors found (HEX only) (True), or don't ... (False)
+writeColorsToFile = True
