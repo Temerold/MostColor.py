63e43d42a28c8b4cb7ea307e9c4ecc929d5541ef
Temerold <67090612+Temerold@users.noreply.github.com>
2020-09-28 17:18:12 +0200

    Hugely improved code

--- MostColor.py
+++ MostColor.py
@@ -1,3 +1,4 @@
+# Imports
 from __future__ import print_function
 from __future__ import division
 import binascii
@@ -8,90 +9,107 @@ import scipy
 import scipy.misc
 import scipy.cluster
 import os
+import pathlib
 
-NUM_CLUSTERS = 5
+from config import *
 
-##### CONFIG
-root = 'A:\Viktigt\Mina program\MostColor.py' # Filepath to check if prompt_Root = True (use '\\' instead of '\')
+# import ctypes
+# import ctypes
+# ctypes.windll.kernel32.SetConsoleTitleW("MostColor")
 
-prompt_Root = False # Program asks for path of images (True), or pre-set root (above) (False)
-prompt_Stop = False # Program says it's done when done (True), or instant close (False)
-write_colors_to_file = True # Program writes all colors found colors (HEX only) (True), or don't ... (False)
-#####
+# Window title
+
+import os
+clear = lambda: os.system("cls")
 
 
-if prompt_Root == True:
+
+if promptRoot:
     root = input("Desired filepath>>> ")
-    
-dots3 = ['.png', '.jpg', '.bmp', '.gif']
-dots4 = ['.webp', '.jpeg']
-cols = []
+
+if writeColorsToFile:
+    cols = []
+
+fileTypes = ['.png', '.jpg', '.bmp', '.webp', '.jpeg']
+
 num = 0
+exist = 0
+failed = 0
 
 for root, dirs, files in os.walk(root, topdown=False):
-    for name in files:
-        if name[-4:] in dots3 or name[-5:] in dots4:
+    for apa in files:
+        if (apa.lower().endswith(tuple(fileTypes))): # Rules for files allowed to search
+            exist += 1
+
+# Major code ..... ;(
+for root, dirs, files in os.walk(root, topdown=False):
+    for name in [fi for fi in files if fi.lower().endswith(tuple(fileTypes))]:
+        try:
+            num += 1
+            im = Image.open(root + "\\" + name)
+            width, height = im.size
+            if debug:
+                print("\nFile #%s:" % (num) +
+                        "\nFile Path: " + root +
+                        "\nFile Name: " + name +
+                        "\nReading image . . ." +
+                        "\nImage size: " + str(width) + " x " + str(height))
+
+
+            ar = np.asarray(im)
+            shape = ar.shape
             try:
-                num += 1
-                print("\nFile #%s:" % num)
-                print("File Path: " + root)
-                print("File Name: " + name)
-                print("\nReading image . . .")
-                im = Image.open(root + "\\" + name)
-             
-                width, height = im.size
-                print("Image size: " + str(width) + " x " + str(height))
-
-                if im.size > (150, 150):
-                    im = im.resize((150, 150))
-                    print("Resized image to 150 x 150.")
-                ar = np.asarray(im)
-                shape = ar.shape
-                try:
-                    ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
-                except:
-                    pass
-
+                ar = ar.reshape(np.product(
+                    shape[:2]), shape[2]).astype(float)
+            except:
+                pass
+            if debug:
                 print("\nFinding clusters . . .")
-                try:
-                    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-                except:
-                    pass
+            try:
+                codes, dist = scipy.cluster.vq.kmeans(ar, 5)
+            except:
+                pass
+            if debug:
                 print("\nCluster centres:\n", codes)
 
+            try:
+                vecs, dist = scipy.cluster.vq.vq(ar, codes)
+            except:
+                pass
+            counts, bins = np.histogram(vecs, len(codes))
+
+            index_max = np.argmax(counts)
+            peak = codes[index_max]
+
+            color = binascii.hexlify(bytearray(int(c)
+                                                for c in peak)).decode('ascii')
+            if color == "00000000":
                 try:
-                    vecs, dist = scipy.cluster.vq.vq(ar, codes)
+                    peak = codes[index_max - 1]
                 except:
                     pass
-                counts, bins = np.histogram(vecs, len(codes))
+                color = binascii.hexlify(
+                    bytearray(int(c) for c in peak)).decode('ascii')
 
+                if debug:
+                    print("\nMost frequent color is %s (#%s)\n\n" %
+                          (peak, color))
 
-                index_max = np.argmax(counts)
-                peak = codes[index_max]
-                
-                color = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
-                if color == "00000000":
-                    try:
-                        peak = codes[index_max - 1]
-                    except:
-                        pass
-                    color = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
-                print("\nMost frequent color is %s (#%s)\n\n" % (peak, color))
-
-                img = Image.new("RGB", (width, height), ImageColor.getrgb("#" + color))
+                img = Image.new("RGB", (width, height),
+                                ImageColor.getrgb("#" + color))
                 os.remove(root + "\\" + name)
                 img.save(root + "\\" + name)
                 # img.show() # don't - may crash computer***
-                
-                if write_colors_to_file == True:
+
+                if writeColorsToFile:
                     cols.append("#" + color)
-            except:
-                print("Operation failed (%s)" % root + "\\" + name)
-                
-    if prompt_Stop == True:
-        input(">>>")
-        
-if write_colors_to_file == True:
+        except:
+            print("Operation failed (%s)" % root + "\\" + name)
+            failed += 1
+
+
+
+if writeColorsToFile:
     try:
         cols = list(dict.fromkeys(cols))
         f = open("colors_hex.csv", 'w')
@@ -99,9 +117,14 @@ if write_colors_to_file == True:
         for col in cols:
             try:
                 f.write(col + '\n')
-                print("\nAppended color to file. ('colors_hex.csv')")
+                print("\nAppended color to file.")
             except:
                 print("\nCouldn't append color to file")
         f.close()
     except:
         print("\nCouldn't append colors to file.")
+
+
+if promptStop:
+    input("Completed %s/%s operations>>>" %
+        (str(exist - failed), str(exist)))
